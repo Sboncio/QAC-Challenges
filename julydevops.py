3afd95e8722f07ed918529fa15b747e05be33305
Sboncio <samuelasquith@btinternet.com>
2020-07-16 16:45:47 +0100

    First challenge

---
+++ julydevops.py
@@ -0,0 +1,8 @@
+cohort = ["Sam", "Tobias", "Steven", "Thembi", "Wasim", "Joshua", "Chris", "Chris", "Arsalan", "Jason", "Domenico", "Ryan", "Jacob", "Mowafak", "Amanda", "Jack",
+"Edmund", "Bradley", "Javas", "Clifford", "Jason", "Bon"]
+
+cohort.append("Luke")
+
+print(cohort[4])
+
+print(cohort.count("Chris"))
