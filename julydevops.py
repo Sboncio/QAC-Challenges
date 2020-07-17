867ac03ee2c9fd9cbe6334ed241068dcf7994713
Sboncio <samuelasquith@btinternet.com>
2020-07-17 08:56:37 +0100

    Name correction

--- julydevops.py
+++ julydevops.py
@@ -1,5 +1,7 @@
-cohort = ["Sam", "Tobias", "Steven", "Thembi", "Wasim", "Joshua", "Chris", "Chris", "Arsalan", "Jason", "Domenico", "Ryan", "Jacob", "Mowafak", "Amanda", "Jack",
-"Edmund", "Bradley", "Javas", "Clifford", "Jason", "Bon"]
+cohort = ["Sam", "Tobias", "Steven", "Thembi", "Wasim", 
+"Joshua", "Chris", "Chris", "Arsalan", "Jason", "Domenico", 
+"Ryan", "Jacob", "Mowafak", "Amanda", "Jack",
+"Edmund", "Bradley", "Javas", "Clifford", "John"]
 
 cohort.append("Luke")
 
