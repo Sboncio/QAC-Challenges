12f5c71f1148ddaf5f07e5a8a6de608821118aec
Samuel Asquith <samuelasquith@btinternet.com>
2020-07-20 19:06:39 +0100

    Create test_string_gen.py

---
+++ test_string_gen.py
@@ -0,0 +1,10 @@
+from programs import string_gen
+
+def test_string_type():
+    assert type(string_gen()) == str
+
+def test_string_len():
+    assert len(string_gen()) == 5
+
+def test_string_case():
+    assert string_gen().islower == True
