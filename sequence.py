7d3f61a5b0611220749a96d4618b3013932159a0
Samuel Asquith <samuelasquith@btinternet.com>
2020-07-22 20:05:43 +0100

    Finishing touches

--- sequence.py
+++ sequence.py
@@ -2,5 +2,9 @@ def order_words(sentence):
     list_of_words = sentence.split(" ")
     words = set(list_of_words)
     words = sorted(words)
-    return words
+    output = ""
+    for word in words:
+        output += word + " "
+    return output
 
+print(order_words("Hello my name is is sam"))
