27415718aef1c11deabd1857dca05eb1f83a675e
Samuel Asquith <samuelasquith@btinternet.com>
2020-07-23 09:57:11 +0100

    Hello from windows

--- sequence.py
+++ sequence.py
@@ -2,8 +2,5 @@ def order_words(sentence):
     list_of_words = sentence.split(" ")
     words = set(list_of_words)
     words = sorted(words)
-    output = ""
-    for word in words:
-        output += word + " "
-    return output
+    return " ".join(words)
 
--- sequence_test.py
+++ sequence_test.py
@@ -1,4 +1,4 @@
 import sequence
 
 def test_remove_and_order():
-    assert sequence.order_words("Hello my name is is sam") == "Hello is my name sam "
+    assert sequence.order_words("Hello my name is is sam") == "Hello is my name sam"
