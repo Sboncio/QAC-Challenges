351f991ea00d16d68ec4e3d6845475d0964c9442
Samuel Asquith <samuelasquith@btinternet.com>
2020-07-24 09:23:59 +0100

    made it cooler

--- CotD2307.py
+++ CotD2307.py
@@ -1,9 +1,10 @@
-def addition(number):
+def addition(number, loops):
     end_num = 0
-    for i in range(1,5):
+    for i in range(1,loops+1):
         concat_num = ""
         for j in range(i):
             concat_num += str(number)
         end_num += int(concat_num)
     return end_num
 
+print(addition(9,5))
